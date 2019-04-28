f0e545fcc19edcb44fc1ff99e4e3ac9a3c55aa22
alicanyilmazz <alicanyilmazz@users.noreply.github.com>
2019-04-29 01:23:04 +0300

    Add project files.

---
+++ Crypto_Using/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Crypto_Using.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string sifresiz = "alican yilmaz";
+        private string sifreli = "AJPq7hnmLZohF/JtTrLa1QwjifkMG2fAKtqcmztZmeIST4vB/IKbrZ3vK+t8Etbzqw==";
+        public ActionResult Index()
+        {
+            string salt = Crypto.GenerateSalt();
+
+            string hash = Crypto.Hash(sifresiz); //Hash e parametre vermezseniz, SHA256 ile aynı şeyi üretir.
+            string hash_md5 = Crypto.Hash(sifresiz, algorithm: "md5");
+            string sh1 = Crypto.SHA1(sifresiz);
+            string sh256 = Crypto.SHA256(sifresiz);
+
+            string sonuc = Crypto.HashPassword(sifresiz);
+            bool dogrumu = Crypto.VerifyHashedPassword(sifreli, sifresiz);
+
+            return View();
+        }
+    }
+}
